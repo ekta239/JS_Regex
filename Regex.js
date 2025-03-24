e966b76416ed28c43e4bb96dbeda561f5d306bce
Ekta verma <ektav8231@gmail.com>
2025-03-25 00:36:33 +0530

    [Ekta]Add.JS RegexCreating Ensure '@' is present and validate the domain

--- Regex.js
+++ Regex.js
@@ -1,7 +1,7 @@
-function validateEmail_UC1(email) {
-    const pattern = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+function validateEmail_UC2(email) {
+    const pattern = /^[a-zA-Z0-9]+@[a-zA-Z]+$/;
     return pattern.test(email);
 }
 
-console.log(validateEmail_UC1("abc@bridgelabz.co")); 
-console.log(validateEmail_UC1("abc@bridgelabz")); 
+console.log(validateEmail_UC2("abc@bridgelabz")); 
+console.log(validateEmail_UC2("abcbridgelabz"));  
